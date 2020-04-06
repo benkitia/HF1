6fc044ddf084b0d2642508aa3d7fe07337c58df1
waffles <kitia.benson@gmail.com>
2020-04-06 17:41:03 -0500

    Added bot information command

--- cogs/basic.py
+++ cogs/basic.py
@@ -15,6 +15,26 @@ class Basic(commands.Cog):
             pass
         t2 = time.perf_counter()
         await ctx.send(":ping_pong: Pong! It took {}ms".format(round((t2-t1)*1000)))
+    
+    @commands.command(description="Returns bot information")
+    async def botinfo(self, ctx):
+        botinfoem = discord.Embed(title="Bot Information",description=f"""
+        {self.bot.user.name} is based on Wafflebot, a moderation and utility bot by Waffle Development written in [discord.py](https://github.com/Rapptz/discord.py)
+        For general support with commands and bot functions join the Waffle Development Discord server: https://discord.gg/zrBqN2v
+        For support with the bot's configuration or to report any bugs or add the bot to your own server join the server above or email support@waffledev.xyz
+        """, color=0x5c92ff)
+        botinfoem.add_field(name = "Docs:", value = "https://waffledev.xyz/wafflebot", inline = False)
+        botinfoem.add_field(name = "Support server:", value = "https://discord.gg/zrBqN2v", inline = False)
+        botinfoem.add_field(name = "Config support and inquiry email:", value = "support@waffledev.xyz", inline = False)
+        botinfoem.add_field(name = "Credits:", value = """
+        waffles#4918 - Main code
+        derw#0387 - Database and infractions
+        Moo#8008 - Database and infractions
+        Blue#9588 - Support with development
+        https://codeclimate.com/github/tekulvw/Squid-Plugins/admin/admin.py/source
+        https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L137
+        """, inline = False)
+        await ctx.send(embed=botinfoem)
 
 def setup(bot):
     bot.add_cog(Basic(bot))
