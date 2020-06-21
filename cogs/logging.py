454a9140b53dbb6254d70daa3f726637b8643217
waffles <kitia.benson@gmail.com>
2020-06-21 00:57:40 -0500

    Overhauled some aspects of logging
    - Very different formatting on all events
    - More info
    - Cleaner code

--- cogs/logging.py
+++ cogs/logging.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import datetime
+import time
 
 class Logging(commands.Cog):
 
@@ -12,43 +13,48 @@ class Logging(commands.Cog):
     async def on_message_delete(self, message):
         ctx: commands.Context = await self.bot.get_context(message)
         getvars = await self.db.guildconfigs.find_one({"_id":ctx.message.guild.id})
-        logchannelidstr = getvars["messagelog"]
-        if logchannelidstr == "disabled" or message.author.bot:
+        logchannelid = getvars["messagelog"]
+        if logchannelid == "disabled" or message.author.bot:
             return
-        logchannelid = int(logchannelidstr)
+        logchannelid = int(logchannelid)
         logchannel = discord.utils.get(ctx.guild.text_channels, id=logchannelid)
         content = ctx.message.content
         if not content:
             content = "[No Content]"
-        msgdellogem = discord.Embed(description=f"Message {ctx.message.id} deleted from <#{ctx.message.channel.id}>:\n**Content:** {content}", color=0xff1919)
-        msgdellogem.timestamp = datetime.datetime.utcnow()
-        msgdellogem.set_author(icon_url=ctx.message.author.avatar_url, name=f"{ctx.message.author} ({ctx.message.author.id})")
+        log = discord.Embed(description=f"{ctx.message.author.mention} deleted a message deleted from {ctx.message.channel.mention}:", color=0xff1919)
+        log.add_field(
+            name = "Content",
+            value = content,
+            inline = False
+            )
+        log.timestamp = datetime.datetime.utcnow()
+        log.set_author(icon_url = ctx.message.author.avatar_url, name=f"{ctx.message.author} ({ctx.message.author.id})")
+        log.set_footer(text = ctx.message.id)
         if ctx.message.attachments:
             attachments = ctx.message.attachments
+            attachmentlist = ""
             for attachment in attachments:
                 url = attachment.url
                 name = attachment.filename
-                if not url.endswith((".jpg", ".jpeg", ".png", ".gif", ".wepb")):
-                    msgdellogem.add_field(name="Attachment",value=name)
-                elif url.endswith((".jpg", ".jpeg", ".png", ".gif", ".wepb")):
-                    cachechannelstr = getvars["imagecache"]
-                    if cachechannelstr == "disabled":
-                        return
-                    if cachechannelstr != "disabled":
-                        cachechannelid = int(cachechannelstr)
-                        cachechannel = discord.utils.get(ctx.guild.text_channels, id=cachechannelid)
-                    await cachechannel.send(attachment.proxy_url)
-                    msgdellogem.add_field(name="Attachment",value=f"[{name}]({attachment.proxy_url})")
-        await logchannel.send(embed=msgdellogem)
+                if not url.endswith((".jpg", ".jpeg", ".png", ".gif", ".wepb",".mp4",".mov",".wmv",".avi",".txt",".docx",".csv",".xlxs")):
+                    attachmentlist = f"{attachmentlist}\n{name}"
+                elif url.endswith((".jpg", ".jpeg", ".png", ".gif", ".wepb",".mp4",".mov",".wmv",".avi",".txt",".docx",".csv",".xlxs")):
+                    attachmentlist = f"{attachmentlist}\n[{name}]({attachment.proxy_url})"
+            log.add_field(
+                name="Attachments",
+                value=attachmentlist,
+                inline = False
+                )
+        await logchannel.send(embed=log)
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         ctx: commands.Context = await self.bot.get_context(before)
         getvars = await self.db.guildconfigs.find_one({"_id":ctx.message.guild.id})
-        logchannelidstr = getvars["messagelog"]
-        if logchannelidstr == "disabled" or before.author.bot:
+        logchannelid = getvars["messagelog"]
+        if logchannelid == "disabled" or before.author.bot:
             return
-        logchannelid = int(logchannelidstr)
+        logchannelid = int(logchannelid)
         logchannel = discord.utils.get(ctx.guild.text_channels, id=logchannelid)
         before_content = before.content
         if not before_content:
@@ -60,67 +66,96 @@ class Logging(commands.Cog):
             return
         if before_content == after_content:
             return
-        msgeditlogem = discord.Embed(description=f"Message {ctx.message.id} edited in <#{ctx.message.channel.id}>:\n**Before:** {before_content}\n**After:** {after_content}", color=0xff8500)
-        msgeditlogem.set_author(icon_url=ctx.message.author.avatar_url, name=f"{ctx.message.author} ({ctx.message.author.id})")
-        msgeditlogem.timestamp = datetime.datetime.utcnow()
-        await logchannel.send(embed=msgeditlogem)
+        log = discord.Embed(
+            description=f"{ctx.message.author.mention} edited a message in {ctx.message.channel.mention}:", 
+            color=0xff8500
+            )
+        log.add_field(
+            name = "Before",
+            value = before_content,
+            inline = False
+        )
+        log.add_field(
+            name = "After",
+            value = after_content,
+            inline = False
+        )
+        log.set_author(icon_url=ctx.message.author.avatar_url, name=f"{ctx.message.author} ({ctx.message.author.id})")
+        log.timestamp = datetime.datetime.utcnow()
+        log.set_footer(text = ctx.message.id)
+        await logchannel.send(embed=log)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         getvars = await self.db.guildconfigs.find_one({"_id":member.guild.id})
-        logchannelidstr = getvars["travellog"]
-        if logchannelidstr == "disabled":
+        logchannelid = getvars["travellog"]
+        if logchannelid == "disabled":
             return
-        logchannelid = int(logchannelidstr)
+        logchannelid = int(logchannelid)
         logchannel = discord.utils.get(member.guild.text_channels, id=logchannelid)
         global_inf_count = await self.db.infractions.count_documents({"Target":member.id, "Status":"Active"})
-        memjoinem = discord.Embed(description=f"{member.name} has joined the server\nThey have {global_inf_count} global infraction(s)\nThere are now {member.guild.member_count} members",color=0x48ff99)
-        memjoinem.set_thumbnail(url=member.avatar_url)
-        memjoinem.timestamp = datetime.datetime.utcnow()
-        memjoinem.set_author(icon_url=member.avatar_url, name=f"{member} ({member.id})")
-        await logchannel.send(embed=memjoinem)
+        dif = (datetime.datetime.utcfromtimestamp(time.time()) - member.created_at)
+        dif = str(dif)
+        print(dif.split(','))
+        dif = dif.split(',')[0]
+        log = discord.Embed(description=f"{member.mention} joined the server\n{global_inf_count} global infraction(s)\nAccount created {dif} ago",color=0x48ff99)
+        log.set_thumbnail(url=member.avatar_url)
+        log.timestamp = datetime.datetime.utcnow()
+        log.set_author(icon_url=member.avatar_url, name=f"{member} ({member.id})")
+        log.set_footer(text = f"Member Count: {member.guild.member_count}")
+        await logchannel.send(embed=log)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         getvars = await self.db.guildconfigs.find_one({"_id":member.guild.id})
-        logchannelidstr = getvars["travellog"]
-        if logchannelidstr == "disabled":
+        logchannelid = getvars["travellog"]
+        if logchannelid == "disabled":
             return
-        logchannelid = int(logchannelidstr)
+        logchannelid = int(logchannelid)
         logchannel = discord.utils.get(member.guild.text_channels, id=logchannelid)
         inf_count = await self.db.infractions.count_documents({"Target":member.id, "Guild":member.guild.id,"Status":"Active"})
-        memleaveem = discord.Embed(description=f"{member.name} has left the server\nThey had {inf_count} infraction(s)\nThere are now {member.guild.member_count} members",color=0xff7d7d)
-        memleaveem.set_thumbnail(url=member.avatar_url)
-        memleaveem.timestamp = datetime.datetime.utcnow()
-        memleaveem.set_author(icon_url=member.avatar_url, name=f"{member} ({member.id})")
-        await logchannel.send(embed=memleaveem)
+        dif = (datetime.datetime.utcfromtimestamp(time.time()) - member.joined_at)
+        dif = str(dif)
+        print(dif.split(','))
+        dif = dif.split(',')[0]
+        log = discord.Embed(
+            description=f"{member.mention} left the server\n{inf_count} local infraction(s)\nStayed for {dif}",
+            color=0xff7d7d
+            )
+        log.set_thumbnail(url=member.avatar_url)
+        log.timestamp = datetime.datetime.utcnow()
+        log.set_author(icon_url=member.avatar_url, name=f"{member} ({member.id})")
+        log.set_footer(text = f"Member Count: {member.guild.member_count}")
+        await logchannel.send(embed=log)
 
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         if before.nick != after.nick:
             getvars = await self.db.guildconfigs.find_one({"_id":before.guild.id})
-            logchannelidstr = getvars["userlog"]
-            if logchannelidstr == "disabled":
+            logchannelid = getvars["userlog"]
+            if logchannelid == "disabled":
                 return
-            logchannelid = int(logchannelidstr)
+            logchannelid = int(logchannelid)
             logchannel = discord.utils.get(before.guild.text_channels, id=logchannelid)
-            nickem = discord.Embed(description=f"Nickname changed\n**Before:** {before.nick}\n**After:** {after.nick}",color=0xeb88ff)
-            nickem.set_author(icon_url=before.avatar_url, name=f"{after} ({after.id})")
-            nickem.timestamp = datetime.datetime.utcnow()
-            await logchannel.send(embed=nickem)
-        # if before.avatar_url != after.avatar_url:
-        #     channel
-        #     getvars = await self.db.guildconfigs.find_one({"_id":before.guild.id})
-        #     logchannelidstr = getvars["user log"]
-        #     if logchannelidstr == "disabled":
-        #         return
-        #     logchannelid = int(logchannelidstr)
-        #     logchannel = discord.utils.get(before.guild.text_channels, id=logchannelid)
-        #     avatarem = discord.Embed(description="Before:\n\n\n\n\n\nAfter:")
-        #     avatarem.set_thumbnail(url=before.avatar_url)
-        #     avatarem.set_image(url=after.avatar_url)
-        #     avatarem.set_author(icon_url=before.avatar_url,name=f"{after} ({after.id})")
-        #     await logchannel.send(embed=avatarem)
+            if before.nick == None:
+                before.nick = "[No nickname]"
+            if after.nick == None:
+                after.nick = "[No nickname]"
+            log = discord.Embed(
+                description=f"{before.mention} updated their nickname:",
+                color=0xeb88ff
+                )
+            log.add_field(
+                name = "Before",
+                value = before.nick
+            )
+            log.add_field(
+                name = "After",
+                value = after.nick
+            )
+            log.set_author(icon_url=before.avatar_url, name=f"{after} ({after.id})")
+            log.timestamp = datetime.datetime.utcnow()
+            await logchannel.send(embed=log)
 
 def setup(bot):
     bot.add_cog(Logging(bot))
