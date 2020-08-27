013e0deb249ce1b5c999abab0834c303ffc39f41
waffles <0@waffles.tech>
2020-08-26 19:12:26 -0500

    Automatic formatting fixes

--- cogs/basic.py
+++ cogs/basic.py
@@ -19,9 +19,9 @@ class Basic(commands.Cog):
         t2 = time.perf_counter()
         await ctx.send(":ping_pong: Pong! It took {}ms".format(round((t2-t1)*1000)))
 
-    @commands.command(description="Returns bot information",aliases=['invite','support'])
+    @commands.command(description="Returns bot information", aliases=['invite', 'support'])
     async def botinfo(self, ctx):
-        botinfoem = discord.Embed(title="Bot Information",description=f"""
+        botinfoem = discord.Embed(title="Bot Information", description=f"""
         Wafflebot is a private moderation and utility bot by Waffle Development written in [discord.py](https://github.com/Rapptz/discord.py).
 
         Wafflebot is currently in the alpha stage. If you'd like to add it to your server, join the support server.
@@ -30,56 +30,64 @@ class Basic(commands.Cog):
 
         For support with the bot's configuration or to report any bugs or add the bot to your own server join the server above or email support@waffledev.xyz
         """, color=0x5c92ff)
-        botinfoem.add_field(name = "Docs:", value = "Coming soon", inline = False)
-        botinfoem.add_field(name = "Support server:", value = "https://discord.gg/zrBqN2v", inline = False)
-        botinfoem.add_field(name = "Config support and inquiry email:", value = "support@waffledev.xyz", inline = False)
-        botinfoem.add_field(name = "Credits:", value = """
+        botinfoem.add_field(name="Docs:", value="Coming soon", inline=False)
+        botinfoem.add_field(name="Support server:",
+                            value="https://discord.gg/zrBqN2v", inline=False)
+        botinfoem.add_field(name="Config support and inquiry email:",
+                            value="support@waffledev.xyz", inline=False)
+        botinfoem.add_field(name="Credits:", value="""
         waffles#4918 - Main code
         derw#0387 - Help with development
         https://codeclimate.com/github/tekulvw/Squid-Plugins/admin/admin.py/source
         https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L137
         https://github.com/AEnterprise/GearBoat
         https://github.com/notderw/penelope
-        """, inline = False)
+        """, inline=False)
         await ctx.send(embed=botinfoem)
 
-    @commands.command(description="Returns a user's avatar in one of many formats",aliases=['avi'])
-    async def avatar(self, ctx, user:discord.Member=None, avi_format='png'):
-        if user==None:
+    @commands.command(description="Returns a user's avatar in one of many formats", aliases=['avi'])
+    async def avatar(self, ctx, user: discord.Member = None, avi_format='png'):
+        if user == None:
             user = ctx.message.author
         try:
             avi = user.avatar_url_as(format=avi_format)
         except:
             return await ctx.send("<:error:696628928458129488> Invalid format. Valid formats include ‘webp’, ‘jpeg’, ‘jpg’, ‘png’ or ‘gif’ (for animated avatars)")
         embed = discord.Embed(
-            title = "Avatar",
-            color = 0x5c92ff
+            title="Avatar",
+            color=0x5c92ff
         )
-        embed.set_author(name = user, icon_url = user.avatar_url)
-        embed.set_footer(text = f"Requested by {ctx.message.author}")
-        embed.set_image(url = avi)
-        await ctx.send(embed = embed)
+        embed.set_author(name=user, icon_url=user.avatar_url)
+        embed.set_footer(text=f"Requested by {ctx.message.author}")
+        embed.set_image(url=avi)
+        await ctx.send(embed=embed)
 
-    @commands.command(description="Returns information about a server",aliases=['server'])
+    @commands.command(description="Returns information about a server", aliases=['server'])
     async def serverinfo(self, ctx, guildid=None):
-        if guildid==None:
+        if guildid == None:
             guildid = ctx.message.guild.id
         guildid = int(guildid)
         guild = self.bot.get_guild(guildid)
         guild = ctx.guild
         guild = self.bot.get_guild(guildid)
-        server_inf_count = await self.db.infractions.count_documents({"Guild":guild.id})
+        server_inf_count = await self.db.infractions.count_documents({"Guild": guild.id})
         findbots = sum(1 for member in ctx.guild.members if member.bot)
         iconurl = guild.icon_url_as(format='png')
-        serverinfoem=discord.Embed(title=guild.name, color=0xFED870)
-        serverinfoem.add_field(name="Server ID", value=ctx.guild.id, inline=True)
-        serverinfoem.add_field(name="Owner", value=ctx.guild.owner, inline=True)
-        serverinfoem.add_field(name="Members", value=ctx.guild.member_count, inline=True)
+        serverinfoem = discord.Embed(title=guild.name, color=0xFED870)
+        serverinfoem.add_field(
+            name="Server ID", value=ctx.guild.id, inline=True)
+        serverinfoem.add_field(
+            name="Owner", value=ctx.guild.owner, inline=True)
+        serverinfoem.add_field(
+            name="Members", value=ctx.guild.member_count, inline=True)
         serverinfoem.add_field(name="Bots", value=findbots, inline=True)
         serverinfoem.add_field(name="Channels", value=len(guild.channels))
-        serverinfoem.add_field(name="Region", value=ctx.guild.region, inline=True)
-        serverinfoem.add_field(name="Verification Level", value=guild.verification_level)
-        serverinfoem.add_field(name="Server Infractions", value=server_inf_count, inline=True)
+        serverinfoem.add_field(
+            name="Region", value=ctx.guild.region, inline=True)
+        serverinfoem.add_field(name="Verification Level",
+                               value=guild.verification_level)
+        serverinfoem.add_field(name="Server Infractions",
+                               value=server_inf_count, inline=True)
         serverinfoem.add_field(name="Created At", value=guild.created_at)
         serverinfoem.set_footer(text=f"Requested by {ctx.message.author}")
         serverinfoem.set_thumbnail(url=iconurl)
