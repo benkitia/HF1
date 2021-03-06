af466f90c64bfa6922b17d71103469d1d06baa13
Benson <benson@kitia.dev>
2021-03-05 21:29:13 -0600

    Config file

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    staff_roles = [
+        663903105133576202
+    ]
+    admin_roles = [
+        429849578326589441
+    ]
+    
+    moderation_log = 449336282511048721
+    message_delete_log = 707867871765594142
+    message_edit_log = 707867871765594142
+    user_join_log = 707867871765594142
+    user_leave_log = 707867871765594142
+    user_nickname_log = 707867871765594142
+
+
+    mute_role = 410604540568666132
+    auto_dehoist = True
