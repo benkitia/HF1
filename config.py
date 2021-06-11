649201b8ff218109fb167b0572beed38d503e5cc
Benson <benson@kitia.dev>
2021-06-11 15:27:22 -0500

    Updated config.py

--- config.py
+++ config.py
@@ -8,14 +8,31 @@ class Config:
         429849578326589441
     ]
     
-    moderation_log = 449336282511048721
-    message_delete_log = 707867871765594142
-    message_edit_log = 707867871765594142
-    user_join_log = 707867871765594142
-    user_leave_log = 707867871765594142
-    user_nickname_log = 707867871765594142
-    photo_of_the_week_channel = 844261014630694942
+    private_inf_log = 449336282511048721
+    public_inf_log = 845914504990556201
+    user_log = 707867871765594142
 
+    embed_colors = {
+        "positive" : 0x43C759,
+        "negative" : 0xF53B30,
+        "log_join" : 0x197AFF,
+        "log_leave" : 0xFCCC00,
+        "log_name" : 0x5AC8FA,
+        "log_message_edit" : 0xF89502,
+        "log_message_delete" : 0xF52C56,
+        "log_infraction" : 0xF53B30
+    }
+
+    log_icon_urls = {
+        "ban" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/ban.png",
+        "kick" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/kick.png",
+        "mute" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/mute.png",
+        "warning" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/warn.png",
+        "unmute" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/unmute.png",
+        "unban" : "https://hair-force-one.s3.us-east-2.amazonaws.com/infraction-logging-thumbnails/unban.png"
+    }
+
+    photo_of_the_week_channel = 844261014630694942
 
     mute_role = 410604540568666132
     auto_dehoist = True
